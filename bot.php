b27c1d35f48d8709726bff21c3f993372fae64b3
Stricted <Stricted@nexus-irc.de>
2012-08-19 17:31:16 +0200

    add config.inc.php

--- bot.php
+++ bot.php
@@ -22,21 +22,7 @@ echo("#### version 1.9-public   ####\n");
 echo("#### coded by Stricted    ####\n");
 echo("##############################\n");
 /* config start */
-$server 		= "localhost";
-$port 			= "8001";
-$botnick 		= "NexusStats";
-$pass 			= "NexusStats:xxxx";
-$admin 			= "Stricted2.user.OnlineGamesNet";
-$logdir 		= "/home/stats/pisg-0.73/log/";
-$cfgdir			= "/home/stats/pisg-0.73/cfg/";
-$statsdir		= "/var/customers/webs/nexus/stats/chan/";
-$archivdir		= "/var/customers/webs/nexus/stats/archiv/";
-$pisgdir		= "/home/stats/pisg-0.73/";
-$botdir			= "/home/stats/";
-$url			= "http://stats.nexus-irc.de/?c=";
-$aurl			= "http://stats.nexus-irc.de/?ac=";
-$defaultlang 	= "EN";
-$trigger		= "?";
+include("config.inc.php");
 /* config end */
 set_time_limit(0);
 $socket = fsockopen($server,$port,$errstr,$errno,2);
@@ -93,5 +79,5 @@ while (true) {
 		
     }
 }
-include("function.php");
+include("function.inc.php");
 ?>
---
+++ config.inc.php
@@ -0,0 +1,17 @@
+<?php
+$server 		= "localhost";
+$port 			= "8001";
+$botnick 		= "NexusStats";
+$pass 			= "NexusStats:xxxx";
+$admin 			= "Stricted2.user.OnlineGamesNet";
+$logdir 		= "/home/stats/pisg-0.73/log/";
+$cfgdir			= "/home/stats/pisg-0.73/cfg/";
+$statsdir		= "/var/customers/webs/nexus/stats/chan/";
+$archivdir		= "/var/customers/webs/nexus/stats/archiv/";
+$pisgdir		= "/home/stats/pisg-0.73/";
+$botdir			= "/home/stats/";
+$url			= "http://stats.nexus-irc.de/?c=";
+$aurl			= "http://stats.nexus-irc.de/?ac=";
+$defaultlang 	= "EN";
+$trigger		= "?";
+??
